651ff0506e5b5c9aefff2063f95967a70a505d29
MyeongHoonNam <codenmh0822@naver.com>
2021-09-14 02:57:38 +0900

    1차 구현 완료

---
+++ src/App.js
@@ -0,0 +1,136 @@
+import Nodes from './Nodes.js';
+import Loading from './Loading.js';
+import ImageViewer from './ImageViewer.js';
+import Breadcrumb from './Breadcrumb.js';
+import { request } from './api.js';
+import { validateState } from './validate.js';
+
+export default function App({ target }) {
+  this.state = {
+    isRoot: true,
+    nodes: [],
+    paths: [],
+    selectedImageUrl: null,
+    isLoading: false,
+  };
+
+  this.setState = (nextState) => {
+    validateState(nextState);
+
+    this.state = nextState;
+
+    nodes.setState({
+      isRoot: this.state.isRoot,
+      nodes: this.state.nodes,
+    });
+
+    imageViewer.setState({
+      selectedImageUrl: this.state.selectedImageUrl,
+    });
+
+    loading.setState(this.state.isLoading);
+
+    breadcrumb.setState(this.state.paths);
+  };
+
+  const loading = new Loading({ target });
+
+  const breadcrumb = new Breadcrumb({
+    target,
+    initialState: this.state.paths,
+    onClick: async (id) => {
+      // 클릭한 경로 외의 path를 제거한다.
+      if (id) {
+        const nextPaths = id ? [...this.state.paths] : [];
+        const pathIndex = nextPaths.findIndex((path) => path.id === id);
+
+        this.setState({
+          ...this.state,
+          paths: nextPaths.slice(0, pathIndex + 1),
+        });
+      } else {
+        this.setState({
+          ...this.state,
+          paths: [],
+        });
+      }
+
+      await fetchNodes(id);
+    },
+  });
+
+  const nodes = new Nodes({
+    target,
+    initialState: {
+      isRoot: this.state.isRoot,
+      nodes: this.state.nodes,
+    },
+    onClick: async (node) => {
+      if (node.type === 'DIRECTORY') {
+        await fetchNodes(node.id);
+
+        this.setState({
+          ...this.state,
+          paths: [...this.state.paths, node],
+        });
+      } else if (node.type === 'FILE') {
+        this.setState({
+          ...this.state,
+          selectedImageUrl: `https://cat-api.roto.codes/static${node.filePath}`,
+        });
+      }
+    },
+    onPrevClick: async () => {
+      if (this.state.isLoading === false) {
+        const nextPaths = [...this.state.paths];
+        nextPaths.pop();
+
+        this.setState({
+          ...this.state,
+          paths: nextPaths,
+        });
+
+        if (nextPaths.length === 0) {
+          await fetchNodes();
+        } else {
+          await fetchNodes(nextPaths[nextPaths.length - 1].id);
+        }
+      }
+    },
+  });
+
+  const imageViewer = new ImageViewer({
+    target,
+    initialState: {
+      selectedImageUrl: this.state.selectedImageUrl,
+    },
+    onClose: () => {
+      this.setState({
+        ...this.state,
+        selectedImageUrl: null,
+      });
+    },
+  });
+
+  const fetchNodes = async (id) => {
+    try {
+      this.setState({
+        ...this.state,
+        isLoading: true,
+      });
+
+      const nodes = await request(id ? `/${id}` : '/');
+
+      this.setState({
+        ...this.state,
+        isRoot: id ? false : true,
+        nodes,
+        isLoading: false,
+      });
+    } catch (e) {
+      alert(e.message);
+    }
+  };
+
+  fetchNodes();
+}
---
+++ src/ImageViewer.js
@@ -0,0 +1,40 @@
+export default function ImageViewer({ target, initialState, onClose }) {
+  const $imageViewer = document.createElement('div');
+  $imageViewer.className = 'ImageViewer Modal';
+
+  target.appendChild($imageViewer);
+
+  this.state = initialState;
+
+  this.setState = (nextState) => {
+    if (this.state.selectedImageUrl !== nextState.selectedImageUrl) {
+      this.state = nextState;
+      this.render();
+    }
+  };
+
+  this.render = () => {
+    $imageViewer.style.display = this.state.selectedImageUrl ? 'block' : 'none';
+
+    $imageViewer.innerHTML = `
+      <div class="content">
+        <img src="${this.state.selectedImageUrl}"/>
+      </div>
+    `;
+  };
+
+  this.render();
+
+  window.addEventListener('keyup', (e) => {
+    // ESC키를 누른경우 onClose 호출
+    if (e.key === 'Escape') {
+      onClose();
+    }
+  });
+
+  $imageViewer.addEventListener('click', (e) => {
+    if (Array.from(e.target.classList).includes('Modal')) {
+      onClose();
+    }
+  });
+}
